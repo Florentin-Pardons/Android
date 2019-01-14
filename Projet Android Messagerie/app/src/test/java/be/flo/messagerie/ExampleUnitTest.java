402b17ed1916700da8933cc62e2901550848c683
Florentin <Flo@gmail.com>
2019-01-14 01:19:31 +0100

    Autre UPD
    Mise a jour autre

--- Projet Android Messagerie/app/src/test/java/be/flo/messagerie/ExampleUnitTest.java
+++ Projet Android Messagerie/app/src/test/java/be/flo/messagerie/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package be.flo.projet_android_messagerie;
+package be.flo.messagerie;
 
 import org.junit.Test;
 
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
 }
